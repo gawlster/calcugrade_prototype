3be9d266ead9aacab6fbeea5789381e90ee0b86d
Connor Gawley (Desktop) <connorgawley@uvic.ca>
2022-06-19 00:30:50 -0600

    init and login route done

---
+++ pages/api/Login.js
@@ -0,0 +1,22 @@
+import { connectToDatabase } from '../../util/mongodb'
+
+export default async (req, res) => {
+    if (req.method !== 'GET')
+        return res
+            .status(405)
+            .json({ message: `Mehtod ${req.method} not allowed on endpoint of type GET` })
+
+    const { db } = await connectToDatabase()
+
+    const curUsernameUser = await db
+        .collection('Users')
+        .find({ username: req.body.username })
+        .toArray()
+
+    if (curUsernameUser.length === 0) return res.status(400).json({ message: 'Invalid username' })
+
+    if (req.body.password !== curUsernameUser.password)
+        return res.status(400).json({ message: 'Incorrect password' })
+
+    res.json(curUsernameUser)
+}
