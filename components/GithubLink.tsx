2f6628ca31b69f8d406dff0b1a9cdc0e41e6cd14
Connor Gawley (Desktop) <connorgawley@uvic.ca>
2022-06-22 16:23:20 -0600

    github link style change

--- components/GithubLink.tsx
+++ components/GithubLink.tsx
@@ -4,7 +4,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const GithubLink: React.FC<{}> = () => {
     return (
-        <div className='w-fit absolute bottom-4 right-1/2 left-1/2'>
+        <div className='w-fit absolute bottom-4 right-1/2 translate-x-1/2'>
             <a
                 className='flex gap-1 font-bold items-center justify-center'
                 href='https://github.com/gawlster/calcugrade'
